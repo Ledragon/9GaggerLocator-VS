b67bd8fe493683d5e981589d18c113d5bf2cf15a
Hugues Stefanski <hugues.stefanski@gmail.com>
2015-05-16 14:56:24 +0200

    framework folder moved

---
+++ 9GaggerLocator/framework/map/worldMap.ts
@@ -0,0 +1,11 @@
+﻿module LeDragon.Framework.Map {
+    export class worldMap {
+        private _group: D3.Selection;
+
+        constructor(container: string|any) {
+            this._group = d3.select(container)
+                .append('g')
+                .classed('map', true);
+        }
+    }
+}
