a7b9c56adbcd49706752ca4fa60829b321004bda
Hugues Stefanski <hugues.stefanski@gmail.com>
2016-04-07 19:05:20 +0200

    WIP

--- 9GaggerLocator/public/app/layout/leftMenu/ldLeftMenu.ts
+++ 9GaggerLocator/public/app/layout/leftMenu/ldLeftMenu.ts
@@ -4,6 +4,21 @@ module app.Layout {
     export class ldLeftMenu implements angular.IDirective {
         restrict = 'E';
         templateUrl = 'app/layout/leftMenu/leftMenu.html';
+        replace = true;
+        controllerAs = 'vm';
+        controller = ldLeftMenuController;
+    }
+
+    export class ldLeftMenuController {
+        static $inject: string[] = ['$scope', 'identityService'];
+        currentUser: any;
+
+        constructor($scope: angular.IScope,
+            identityService: Services.IidentityService) {
+            $scope.$watch(() => identityService.currentUser, () => {
+                this.currentUser = identityService.currentUser;
+            });
+        }
     }
 
     angular.module('app')
